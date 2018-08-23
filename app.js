536f505440806d6dc7b5842416ae92fbb30eb9eb
whiDev <whiplashoo721@gmail.com>
2018-08-23 17:03:19 +0300

    test https

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var compression = require('compression');
 var helmet = require('helmet');
 
 var index = require('./routes/index');
+var http = require('http');
+var enforce = require('express-sslify');
 
 var app = express();
 
@@ -30,6 +32,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
 
 app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
