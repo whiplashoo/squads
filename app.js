192d9580f725498eba0424503bb6b90bc885cddb
whiDev <whiplashoo721@gmail.com>
2018-11-30 20:32:29 +0200

    ads22

--- app.js
+++ app.js
@@ -32,11 +32,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(enforce.HTTPS({ trustProtoHeader: true }))
+// app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+// http.createServer(app).listen(app.get('port'), function() {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
 
 app.use(compression()); //Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
