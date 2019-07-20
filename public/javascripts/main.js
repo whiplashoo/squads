def090083ca5a443f76602ef5a146546e24503db
whiDev <whiplashoo721@gmail.com>
2019-07-20 12:35:57 +0300

    notice about transfer window updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -399,4 +399,12 @@ $('#formation-title').change(function() {
     updateTitle($(this).val());
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
 
