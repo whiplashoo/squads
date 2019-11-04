bf07dcd0805388f9a3e0703b5b09b0b801112bb8
whiDev <whiplashoo721@gmail.com>
2019-11-04 13:07:47 +0200

    clean-up, let/const instead of var, removed finalCanvas

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,7 @@
-var currentData = {};
+let playersOnDraft = {};
+// #TODO:
+// - Check responsiveness
+// - Drag and drop?
 
 $(document).ready(function() {
 
@@ -25,28 +28,21 @@ $(document).ready(function() {
         templateSelection: optionData
     });
 
-
     $(".search-entity").on("select2:select", function(e) {
-        var selected = e.params.data;
-        var id = $(this).data('id');
-        var targetX = parseInt($(this).data('cx'));
-        var targetY = parseInt($(this).data('cy'));
+        const selected = e.params.data;
+        const id = $(this).data('id');
+        const targetX = parseInt($(this).data('cx'));
+        const targetY = parseInt($(this).data('cy'));
 
-        var imgURL = "https://d3obiipglq02d0.cloudfront.net/" + selected.s3url + ".png";
-        var playerName = selected.text;
+        const imgURL = "https://d3obiipglq02d0.cloudfront.net/" + selected.s3url + ".png";
+        const playerName = selected.text;
 
-        currentData[id] = { "imgURL": imgURL, "name": playerName, "s3url": selected.s3url };
+        playersOnDraft[id] = { "imgURL": imgURL, "name": playerName, "s3url": selected.s3url };
 
         upsertImage(id, imgURL, playerName);
 
         $(this).val('').trigger('change');
         $(this).parent().siblings('.search-selected').html(selected.text);
-    });
-
-    $(".search-entity").on("select2:unselect", function(e) {
-        var selected = e.params.data;
-        var id = $(this).data('id');
-        $(this).parent().siblings('.search-selected').html('');
     });
 
     $("#formationSelect").change(function() {
@@ -56,8 +52,8 @@ $(document).ready(function() {
 });
 
 function upsertImage(id, imgURL, playerName) {
-    var $draftImg;
-    var $draftLabel;
+    let $draftImg;
+    let $draftLabel;
     // If there is already an img for this position on the draft
     if ($("#draft-img-" + id).length) {
         $draftImg = $("#draft-img-" + id);
@@ -73,8 +69,6 @@ function upsertImage(id, imgURL, playerName) {
     $("#draft-pos-" + id).hide();
 }
 
-
-
 function optionData(data, container) {
     $(data.element).attr("data-s3url", data.s3url);
     $(data.element).attr("data-pos", data.pos);
@@ -83,15 +77,15 @@ function optionData(data, container) {
 
 function template(data, container) {
     if (data.text && data.s3url) {
-        var pImg = '<div class="column is-one-third"><img crossorigin="anonymous" class="player-img" src="https://d3obiipglq02d0.cloudfront.net/' + data.s3url + '.png"/></div>';
-        var pName = '<div class="column"><p class="player-name"><strong>' + data.text + '</strong></p>';
-        var pDetails = '<p class="player-details">' + data.club + ' , ' + data.age + ' , ' + data.pos + '</p></div>';
+        const pImg = '<div class="column is-one-third"><img crossorigin="anonymous" class="player-img" src="https://d3obiipglq02d0.cloudfront.net/' + data.s3url + '.png"/></div>';
+        const pName = '<div class="column"><p class="player-name"><strong>' + data.text + '</strong></p>';
+        const pDetails = '<p class="player-details">' + data.club + ' , ' + data.age + ' , ' + data.pos + '</p></div>';
         return '<div class="columns">' + pImg + pName + pDetails + '</div>';
     }
 }
 
 function updateTitle(newTitle) {
-    var $draftTitle;
+    let $draftTitle;
     if ($("#draft-title").length) {
         $draftTitle = $("#draft-title");
     } else {
@@ -107,57 +101,57 @@ function updateTitle(newTitle) {
 
 function drawPositionCircle(ctx, posName, destX, destY, color) {
     ctx.beginPath();
-    ctx.arc(destX + 50, destY + 50, 20, 0, 2 * Math.PI, false);
+    ctx.arc(destX + 100, destY + 100, 40, 0, 2 * Math.PI, false);
     ctx.fillStyle = circleColors[posName];
     ctx.fill();
-    ctx.lineWidth = 1;
+    ctx.lineWidth = 2;
     ctx.strokeStyle = color;
     ctx.stroke();
-    ctx.font = "bold 14px system-ui";
+    ctx.font = "bold 28px system-ui";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
-    ctx.fillText(posName.toUpperCase(), destX + 50, destY + 55);
+    ctx.fillText(posName.toUpperCase(), destX + 100, destY + 110);
     ctx.closePath();
 }
 
 function updateDraft() {
-    let draftW = $('#draft').width();
-    let draftH = $('#draft').height();
-    var positions = $(".position-label");
-    var draftContainers = $(".draft-container");
-    for (var id = 0; id < 11; id++) {
-        var newPos = currentFormation[id];
-        var destX = newPos.cx;
-        var destY = newPos.cy;
-        var posName = newPos.pos;
+    const draftW = $('#draft').width();
+    const draftH = $('#draft').height();
+    const positions = $(".position-label");
+    const draftContainers = $(".draft-container");
+    for (let id = 0; id < 11; id++) {
+        const newPos = currentFormation[id];
+        const destX = newPos.cx;
+        const destY = newPos.cy;
+        const posName = newPos.pos;
 
         $(draftContainers[id]).css({
             "left": destX + "%",
             "top": destY + "%",
-        })
+        });
         
+        let $draftPos;
         if ($("#draft-pos-" + id).length) {
-            var $draftPos = $("#draft-pos-" + id);
+            $draftPos = $("#draft-pos-" + id);
         } else {
-            var $draftPos = $("<div class='draft-pos' id='draft-pos-" + id + "'> </div>");
+            $draftPos = $("<div class='draft-pos' id='draft-pos-" + id + "'> </div>");
             $(draftContainers[id]).append($draftPos);
         }
         
-        $draftPos.html(posName).css({
-            "background-color": circleColors[posName]
-        });
+        $draftPos.css("background-color", circleColors[posName]).html(posName);
+        $(positions[id]).css("background-color", circleColors[posName]).html(posName);
     }
 }
 
 function prepareCanvas() {
-    var canvas = document.getElementById("canvas1");
-    var ctx = canvas.getContext("2d");
-    var padd = 30;
-    var w = canvas.width; //500
-    var h = canvas.height; //700
-    var selects = $(".search-entity");
-    var positions = $(".position-label");
-    var title = $("#draft-title").html();
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    const padd = 60;
+    const w = canvas.width; //1000
+    const h = canvas.height; //1400
+    const selects = $(".search-entity");
+    const positions = $(".position-label");
+    const title = $("#draft-title").html();
 
     // -- START DRAWING THE FIELD --
     ctx.fillStyle = "#52AC5B";
@@ -173,90 +167,90 @@ function prepareCanvas() {
     ctx.lineTo(padd, padd);
 
     // -- Top Goal Post --
-    ctx.moveTo(220, padd);
-    ctx.lineTo(220, padd - padd / 2);
-    ctx.lineTo(280, padd - padd / 2);
-    ctx.lineTo(280, padd);
+    ctx.moveTo(440, padd);
+    ctx.lineTo(440, padd - padd / 2);
+    ctx.lineTo(560, padd - padd / 2);
+    ctx.lineTo(560, padd);
 
     // -- Bot Goal Post --
-    ctx.moveTo(220, h - padd);
-    ctx.lineTo(220, h - padd + padd / 2);
-    ctx.lineTo(280, h - padd + padd / 2);
-    ctx.lineTo(280, h - padd);
+    ctx.moveTo(440, h - padd);
+    ctx.lineTo(440, h - padd + padd / 2);
+    ctx.lineTo(560, h - padd + padd / 2);
+    ctx.lineTo(560, h - padd);
 
     // -- Top Penalty Area --
-    ctx.moveTo(125, padd);
-    ctx.lineTo(125, padd + 100);
-    ctx.lineTo(375, padd + 100);
-    ctx.lineTo(375, padd);
-    ctx.moveTo(200, padd);
-    ctx.lineTo(200, padd + 35);
-    ctx.lineTo(300, padd + 35);
-    ctx.lineTo(300, padd);
+    ctx.moveTo(250, padd);
+    ctx.lineTo(250, padd + 200);
+    ctx.lineTo(750, padd + 200);
+    ctx.lineTo(750, padd);
+    ctx.moveTo(400, padd);
+    ctx.lineTo(400, padd + 70);
+    ctx.lineTo(600, padd + 70);
+    ctx.lineTo(600, padd);
 
     // -- Bot Penalty Area --
-    ctx.moveTo(125, h - padd);
-    ctx.lineTo(125, h - padd - 100);
-    ctx.lineTo(375, h - padd - 100);
-    ctx.lineTo(375, h - padd);
-    ctx.moveTo(200, h - padd);
-    ctx.lineTo(200, h - padd - 35);
-    ctx.lineTo(300, h - padd - 35);
-    ctx.lineTo(300, h - padd);
+    ctx.moveTo(250, h - padd);
+    ctx.lineTo(250, h - padd - 200);
+    ctx.lineTo(750, h - padd - 200);
+    ctx.lineTo(750, h - padd);
+    ctx.moveTo(400, h - padd);
+    ctx.lineTo(400, h - padd - 70);
+    ctx.lineTo(600, h - padd - 70);
+    ctx.lineTo(600, h - padd);
 
     // -- Middle Line --
     ctx.moveTo(padd, h / 2);
     ctx.lineTo(w - padd, h / 2);
 
     // -- Circles and semi-circles --
-    ctx.moveTo(200, padd + 100);
-    ctx.quadraticCurveTo(w / 2, padd + 135, 300, padd + 100)
-    ctx.moveTo(200, h - padd - 100);
-    ctx.quadraticCurveTo(w / 2, h - padd - 135, 300, h - padd - 100)
+    ctx.moveTo(400, padd + 200);
+    ctx.quadraticCurveTo(w / 2, padd + 270, 600, padd + 200)
+    ctx.moveTo(400, h - padd - 200);
+    ctx.quadraticCurveTo(w / 2, h - padd - 270, 600, h - padd - 200)
     ctx.moveTo(w / 2, h / 2);
     ctx.arc(w / 2, h / 2, 60, 0, Math.PI * 2, true);
 
     // -- Corner quarter-circles --
     ctx.moveTo(padd, padd);
-    ctx.arc(padd, padd, 8, 0, Math.PI / 2, false);
+    ctx.arc(padd, padd, 32, 0, Math.PI / 2, false);
     ctx.moveTo(w - padd, padd);
-    ctx.arc(w - padd, padd, 8, Math.PI / 2, Math.PI, false);
+    ctx.arc(w - padd, padd, 32, Math.PI / 2, Math.PI, false);
     ctx.moveTo(w - padd, h - padd);
-    ctx.arc(w - padd, h - padd, 8, Math.PI, Math.PI * 1.5, false);
+    ctx.arc(w - padd, h - padd, 32, Math.PI, Math.PI * 1.5, false);
     ctx.moveTo(padd, padd);
-    ctx.arc(padd, h - padd, 8, Math.PI * 1.5, 0, false);
+    ctx.arc(padd, h - padd, 32, Math.PI * 1.5, 0, false);
 
     ctx.moveTo(0, 0);
     ctx.closePath();
-    ctx.lineWidth = 3;
+    ctx.lineWidth = 6;
     ctx.stroke();
     // -- END DRAWING THE FIELD --
 
-    ctx.font = "10px arial";
+    ctx.font = "20px arial";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
-    ctx.fillText("Created with CreateFormation.com", 410, 695);
+    ctx.fillText("Created with CreateFormation.com", 820, 1390);
 
-    for (var i = 0; i < 11; i++) {
-        var newPos = currentFormation[i];
-        var sel = selects[i];
-        var player = currentData[i.toString()];
+    for (let i = 0; i < 11; i++) {
+        const newPos = currentFormation[i];
+        const sel = selects[i];
+        const player = playersOnDraft[i.toString()];
 
-        var destX = newPos.cx * w / 100;
-        var destY = newPos.cy * h / 100;
-        var posName = newPos.pos;
+        const destX = newPos.cx * w / 100;
+        const destY = newPos.cy * h / 100;
+        const posName = newPos.pos;
 
         //  If the select2 select has a value, draw an image, else a simple circle.
         if (player && player.imgURL) {
-            var img = document.getElementById("draft-img-" + i);
-            ctx.drawImage(img, destX, destY, 100, 100);
+            const img = document.getElementById("draft-img-" + i);
+            ctx.drawImage(img, destX, destY, 200, 200);
 
             ctx.fillStyle = "#444";
-            ctx.fillRect(destX + 10, destY + 100, 80, 20);
-            ctx.font = "14px Fira Sans";
+            ctx.fillRect(destX + 20, destY + 200, 160, 40);
+            ctx.font = "28px Fira Sans";
             ctx.fillStyle = "white";
             ctx.textAlign = "center";
-            ctx.fillText(player.name.split(" ").splice(-1), destX + 50, destY + 115);
+            ctx.fillText(player.name.split(" ").splice(-1), destX + 100, destY + 230);
         } else {
             drawPositionCircle(ctx, posName, destX, destY, "#fff");
         }
@@ -270,32 +264,30 @@ function prepareCanvas() {
 
     if (title) {
         ctx.fillStyle = "#444";
-        ctx.fillRect(0, 0, 200, 20);
-        ctx.font = "12px Fira Sans";
+        ctx.fillRect(0, 0, 400, 40);
+        ctx.font = "24px Fira Sans";
         ctx.fillStyle = "#f0f600";
         ctx.textAlign = "left";
-        ctx.fillText(title, 10, 15);
+        ctx.fillText(title, 20, 30);
     }   
-    // -- END SETTING SELECTS VALUES --
-
 }
 
 $(".position").hover(function() {
     $(this).addClass('focused');
-    var select = $(this).find('.search-entity');
-    var id = select.data('id');
+    const select = $(this).find('.search-entity');
+    const id = select.data('id');
     $("#draft-pos-" + id).addClass('focused');
 }, function() {
     $(this).removeClass('focused');
-    var select = $(this).find('.search-entity');
-    var id = select.data('id');
+    const select = $(this).find('.search-entity');
+    const id = select.data('id');
     $("#draft-pos-" + id).removeClass('focused');
 });
 
 $(".remove-player").on("click", function() {
-    var $positionDiv = $(this).parent().parent();
-    var id = $positionDiv.find('.search-entity').data('id');
-    delete currentData[id];
+    const $positionDiv = $(this).parent().parent();
+    const id = $positionDiv.find('.search-entity').data('id');
+    delete playersOnDraft[id];
 
     $("#draft-label-" + id).remove();
     $("#draft-img-" + id).remove();
@@ -305,46 +297,47 @@ $(".remove-player").on("click", function() {
     $positionDiv.find('.search-entity').val("").trigger("change");
 });
 
-
 /********************
 **  SAVE/LOAD CONFIG
 ********************/
 function saveConfig() {
     let saveData = { title: $('#formation-title').val(), formation: $("#formationSelect").val() };
-    for (let p in currentData) {
-        let player = currentData[p];
+    for (let p in playersOnDraft) {
+        let player = playersOnDraft[p];
         saveData[p] = {name: player.name, id: player.s3url};
     }
     return JSON.stringify(saveData);
 }
 
 function loadConfig(loadData) {
-    $('#formation-title').val(loadData.title).trigger("change");
+    $("#formation-title").val(loadData.title).trigger("change");
     $("#formationSelect").val(loadData.formation).trigger("change");
+    // Initialize players
+    $(".remove-player").trigger("click");
 
-    for (let i=0; i<11; i++) {
+    for (let i = 0; i < 11; i++) {
         if (loadData[i]) {
             let p = loadData[i];
             loadPlayer(p.id, i);
-        }
+        } 
     }
 }
 
 function loadPlayer(s3url, id) {
     // Fetch the preselected item, and add to the control
-    var playerSelect = $('.search-entity[data-id="' + id + '"]');
+    const playerSelect = $('.search-entity[data-id="' + id + '"]');
     $.ajax({
         type: 'GET',
         url: '/api/player/p/' + s3url
     }).then(function (data) {
 
-        var option = new Option(data.name, data.id, true, true);
+        const option = new Option(data.name, data.id, true, true);
         playerSelect.append(option).trigger('change');
 
-        var imgURL = "https://d3obiipglq02d0.cloudfront.net/" + s3url + ".png?crossorigin";
-        var playerName = data.name;
+        const imgURL = "https://d3obiipglq02d0.cloudfront.net/" + s3url + ".png?crossorigin";
+        const playerName = data.name;
 
-        currentData[id] = { "name": playerName, "imgURL": imgURL, "s3url": s3url };
+        playersOnDraft[id] = { "name": playerName, "imgURL": imgURL, "s3url": s3url };
 
         playerSelect.parent().siblings('.search-selected').html(data.name);
 
@@ -378,18 +371,7 @@ $("#download").on("click", function() {
     prepareCanvas();
     var canvas = document.getElementById("canvas1");
     var ctx = canvas.getContext("2d");
-
-    var finalCanvas = document.getElementById("finalCanvas");
-    var fctx = finalCanvas.getContext("2d");
-    fctx.clearRect(0, 0, finalCanvas.width, finalCanvas.height);
-    fctx.resetTransform();
-    fctx.webkitImageSmoothingEnabled = false;
-    fctx.mozImageSmoothingEnabled = false;
-    fctx.imageSmoothingEnabled = false;
-    fctx.scale(2, 2);
-    fctx.drawImage(canvas, 0, 0);
-
-    this.href = finalCanvas.toDataURL();
+    this.href = canvas.toDataURL();
     var d = new Date();
     var dateString = d.toLocaleString().replace(/[^\w\s]/gi, '_');
     this.download = $('#formation-title').val() || "formation_" + dateString;
